2b165018ceca157bac1b3d322010a62d9ecb2d67
xuebeibei <xuebei@seimun.com>
2017-12-05 14:35:08 +0800

    Add - core中添加自定义表格控件

---
+++ GUI/HISGUICore/MyContorls/MyTableEdit.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HISGUICore.MyContorls
+{
+    /// <summary>
+    /// MyTableEdit.xaml 的交互逻辑
+    /// </summary>
+    public partial class MyTableEdit : UserControl
+    {
+        public MyTableEdit()
+        {
+            InitializeComponent();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveTempletBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PrintBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
