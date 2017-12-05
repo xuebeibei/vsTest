4cd4149a0beb0ac53cf5add417d883575292e74b
xuebeibei <xuebei@seimun.com>
2017-12-05 14:05:55 +0800

    Add - 添加病历控件的智能搜索（一半）

--- GUI/HISGUICore/MyContorls/MyTextEdit.xaml.cs
+++ GUI/HISGUICore/MyContorls/MyTextEdit.xaml.cs
@@ -15,78 +15,49 @@ using System.Windows.Shapes;
 
 namespace HISGUICore.MyContorls
 {
-    public class MyTextEditText : DependencyObject
-    {
-        public string Tittle
-        {
-            get { return (string)GetValue(TittleProperty); }
-            set { SetValue(TittleProperty, value); }
-        }
-
-        public static readonly DependencyProperty TittleProperty =
-            DependencyProperty.Register("Tittle", typeof(string), typeof(MyTextEditText));
-
-        public string Tip
-        {
-            get { return (string)GetValue(TipProperty); }
-            set { SetValue(TipProperty, value); }
-        }
-
-        public static readonly DependencyProperty TipProperty =
-            DependencyProperty.Register("Tip", typeof(string), typeof(MyTextEditText));
-
-        public string Text
-        {
-            get { return (string)GetValue(TextProperty); }
-            set { SetValue(TextProperty, value); }
-        }
-
-        public static readonly DependencyProperty TextProperty =
-            DependencyProperty.Register("Text", typeof(string), typeof(MyTextEditText));
-
-        public int MaxNum
-        {
-            get { return (int)GetValue(MaxNumProperty); }
-            set { SetValue(MaxNumProperty, value); }
-        }
-
-        public static readonly DependencyProperty MaxNumProperty =
-            DependencyProperty.Register("MaxNum", typeof(int), typeof(MyTextEditText));
-
-        public int TextNum
-        {
-            get { return (int)GetValue(TextNumProperty); }
-            set { SetValue(TextNumProperty, value); }
-        }
-
-        public static readonly DependencyProperty TextNumProperty =
-            DependencyProperty.Register("TextNum", typeof(int), typeof(MyTextEditText));
-
-        public BindingExpressionBase SetBinding(DependencyProperty dp, BindingBase binding)
-        {
-            return BindingOperations.SetBinding(this, dp, binding);
-        }
-    }
-
     public partial class MyTextEdit : UserControl
     {
-        //private MyTextEditText mytext;
-        //public MyTextEditText mytext { get; set; }
-
         public MyTextEdit()
         {
             InitializeComponent();
-            //mytext = new MyTextEditText();
-            //mytext.SetValue(MyTextEditText.TittleProperty, "aaaa:");
-            //mytext.SetValue(MyTextEditText.TextProperty, "dfadfasdfdasfdsadsf");
-            //mytext.SetValue(MyTextEditText.MaxNumProperty, 500);
+        }
 
-            //this.TittleLabel.SetBinding(Label.ContentProperty, new Binding("Tittle") { Source = mytext });
-            //this.TextEdit.SetBinding(TextBox.TextProperty, new Binding("Text") { Source = mytext });
-            //this.TextNum.SetBinding(Label.ContentProperty, new Binding("TextNum") { Source = mytext});
-            //this.TextMaxNum.SetBinding(Label.ContentProperty, new Binding("MaxNum") { Source = mytext });
+        public MyTextEdit(string strTittle, int maxnum)
+        {
+            InitializeComponent();
+            this.TittleLabel.Content = strTittle;
+            this.TextMaxNum.Content = maxnum.ToString();
+            this.TextEdit.MaxLength = maxnum;
+        }
 
+        private void TextEdit_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter || e.Key == Key.Return)
+            {
+                string str = TextEdit.Text;
+                string strSelect = "";
+                if (string.IsNullOrEmpty(str))
+                {
+                    return;
+                }
+                if (str.IndexOf(';') == -1)
+                {
+                    strSelect = str;
+                }
+                else
+                {
+                    string[] p = str.Split(';');
 
+                    if (p == null)
+                        return;
+                    if (p.Count() <= 0)
+                        return;
+
+                    strSelect = p[p.Count() - 1];
+                }
+                //MessageBox.Show(strSelect);
+
+            }
         }
     }
 }
