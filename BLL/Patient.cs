17c648b35c4c098f14743445963ba5fb9035c938
xuebeibei <xuebei@seimun.com>
2017-12-12 15:43:41 +0800

    Delete - 删除无用的代码

--- BLL/Patient.cs
+++ BLL/Patient.cs
@@ -9,10 +9,5 @@ namespace BLL
     public class Patient
     {
         CommContracts.Patient patient;
-
-        public Patient(CommContracts.Patient patient)
-        {
-            this.patient = patient;
-        }
     }
 }
