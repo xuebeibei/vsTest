7866e8748093e4fc3bab715593ae4da452dce9cc
xuebeibei <xuebei@seimun.com>
2017-12-12 15:29:49 +0800

    Add - 添加处方的类和处方正文类

---
+++ Contracts/Recipe.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace CommContracts
+{
+    public enum RecipeTypeEnum { XiChengYao, ZhaoYao };
+
+    [DataContract]
+    public class Recipe
+    {
+        public Recipe()
+        {
+            this.RecipeTypeEnum = RecipeTypeEnum.XiChengYao;
+        }
+
+        [DataMember]
+        public int ID { get; set; }                               // 处方ID
+        [DataMember]
+        public string No { get; set; }                            // 处方编号
+        [DataMember]
+        public RecipeTypeEnum RecipeTypeEnum { get; set; }        // 处方类型
+        [DataMember]
+        public string MedicalInstitution { get; set; }            // 医疗机构名称
+        [DataMember]
+        public int ChargeTypeEnum { get; set; }                   // 费别,*是否存在在门诊和住院中，待定
+        [DataMember]
+        public int RegistrationID { get; set; }                   // 门诊ID
+        [DataMember]
+        public int InpatientID { get; set; }                      // 住院ID
+        [DataMember]
+        public string ClinicalDiagnosis { get; set; }             // 临床诊断
+        [DataMember]
+        public string PatientsIDCardNum { get; set; }             // 患者身份证    -- 麻醉和精一处方
+        [DataMember]
+        public string ProxyIDCardNum { get; set; }                // 代办人身份证  -- 麻醉和精一处方
+        [DataMember]
+        public string ProxyName { get; set; }                     // 代办人姓名    -- 麻醉和精一处方
+        [DataMember]
+        public double SumOfMoney { get; set; }                    // 金额
+        [DataMember]
+        public DateTime WriteTime { get; set; }                   // 开具时间
+        [DataMember]
+        public int WriteUserID { get; set; }                      // 开具医生
+        [DataMember]
+        public int AuditorUserID { get; set; }                    // 审核、调配，核对、发药人员
+        [DataMember]
+        public DateTime AuditorTime { get; set; }                 // 审核、调配，核对、发药时间
+    }
+}
---
+++ Contracts/RecipeDetails.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace CommContracts
+{
+    [DataContract]
+    public class RecipeDetails
+    {
+        public RecipeDetails()
+        {
+
+        }
+
+        [DataMember]
+        public int ID { get; set; }                               // 处方正文ID
+        [DataMember]
+        public string GroupNum { set; get; }                      // 组别
+        [DataMember]
+        public int DrugID { get; set; }                           // 药品ID
+        [DataMember]
+        public int SingleDose { get; set; }                       // 单次剂量
+        [DataMember]
+        public string Usage { get; set; }                         // 用法
+        [DataMember]
+        public string DDDS { get; set; }                          // 使用频率
+        [DataMember]
+        public int DaysNum { get; set; }                          // 天数
+        [DataMember]
+        public int IntegralDose { get; set; }                     // 总量
+        [DataMember]
+        public string Illustration { get; set; }                  // 说明
+        [DataMember]
+        public int RecipeID { get; set; }                         // 所属处方ID
+    }
+}
