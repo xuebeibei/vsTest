1f0910a70159bb2349b2d76928a447fb290af2c6
xuebeibei <xuebei@seimun.com>
2017-12-12 15:44:26 +0800

    Add - Bll添加类

---
+++ BLL/Recipe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Data.Entity;
+using System.Data;
+using System.Globalization;
+using System.Collections;
+
+namespace BLL
+{
+    public class Recipe
+    {
+
+    }
+}
