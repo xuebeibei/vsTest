d18b590ce94309ece5b5701e441a8b4fac4b4845
xuebeibei <xuebei@seimun.com>
2017-12-20 11:01:41 +0800

    Add - 添加查询药品

---
+++ BLL/Medicine.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class Medicine
+    {
+        public List<CommContracts.Medicine> getAllMedicine()
+        {
+            List<CommContracts.Medicine> list = new List<CommContracts.Medicine>();
+            using (DAL.HisContext ctx = new DAL.HisContext())
+            {
+                var query = from m in ctx.Medicines
+                            select m;
+                var config = new MapperConfiguration(cfg =>
+                {
+                    cfg.CreateMap<DAL.Medicine, CommContracts.Medicine>();
+                });
+                var mapper = config.CreateMapper();
+
+                foreach (DAL.Medicine tem in query)
+                {
+                    var dto = mapper.Map<CommContracts.Medicine>(tem);
+                    list.Add(dto);
+                }
+            }
+            return list;
+        }
+        
+    }
+}
