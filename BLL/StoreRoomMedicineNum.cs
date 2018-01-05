c7236b67739af936f240284727356b7c1e882359
xuebeibei <xuebei@seimun.com>
2018-01-05 15:04:16 +0800

    Add - 同一个库房中相同批次相同有效期相同进价的相同药品数量合并

--- BLL/StoreRoomMedicineNum.cs
+++ BLL/StoreRoomMedicineNum.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,15 +23,33 @@ namespace BLL
                 {
                     if (tempDetail == null)
                         continue;
-                    DAL.StoreRoomMedicineNum storeRoomMedicineNum = new DAL.StoreRoomMedicineNum();
-                    storeRoomMedicineNum.Batch = tempDetail.Batch;
-                    storeRoomMedicineNum.MedicineID = tempDetail.MedicineID;
-                    storeRoomMedicineNum.StoreRoomID = medicineInStore.ToStoreID;
-                    storeRoomMedicineNum.Num = tempDetail.Num;
-                    storeRoomMedicineNum.SellPrice = tempDetail.SellPrice;
-                    storeRoomMedicineNum.StorePrice = tempDetail.StorePrice;
-                    storeRoomMedicineNum.ExpirationDate = tempDetail.ExpirationDate;
-                    ctx.StoreRoomMedicineNums.Add(storeRoomMedicineNum);
+
+                    var query = from s in ctx.StoreRoomMedicineNums
+                                where s.Batch == tempDetail.Batch &&
+                                s.MedicineID == tempDetail.MedicineID &&
+                                s.StoreRoomID == medicineInStore.ToStoreID &&
+                                s.StorePrice == tempDetail.StorePrice &&
+                                s.ExpirationDate == tempDetail.ExpirationDate
+                                select s;
+
+                    
+                    if (query.Count() == 0)
+                    {
+                        DAL.StoreRoomMedicineNum storeRoomMedicineNum = new DAL.StoreRoomMedicineNum();
+                        storeRoomMedicineNum.Batch = tempDetail.Batch;
+                        storeRoomMedicineNum.MedicineID = tempDetail.MedicineID;
+                        storeRoomMedicineNum.StoreRoomID = medicineInStore.ToStoreID;
+                        storeRoomMedicineNum.Num = tempDetail.Num;
+                        storeRoomMedicineNum.SellPrice = tempDetail.SellPrice;
+                        storeRoomMedicineNum.StorePrice = tempDetail.StorePrice;
+                        storeRoomMedicineNum.ExpirationDate = tempDetail.ExpirationDate;
+                        ctx.StoreRoomMedicineNums.Add(storeRoomMedicineNum);
+                    }
+                    else
+                    {
+                        var temp = query.First();
+                        temp.Num += tempDetail.Num;
+                    }
                 }
 
                 try
