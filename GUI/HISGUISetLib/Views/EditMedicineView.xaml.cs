30b038145dc7e4a046ab9cb76f664bc96be73fca
xubei-PC\xubei <xuebei@seimun.com>
2018-01-17 14:28:03 +0800

    Add - 添加对药品的其他属性的编辑和保存

--- GUI/HISGUISetLib/Views/EditMedicineView.xaml.cs
+++ GUI/HISGUISetLib/Views/EditMedicineView.xaml.cs
@@ -26,6 +26,8 @@ namespace HISGUISetLib.Views
         {
             InitializeComponent();
             MedicineTypeEnumCombo.ItemsSource = Enum.GetValues(typeof(CommContracts.MedicineTypeEnum));
+            DosageFormEnumCombo.ItemsSource = Enum.GetValues(typeof(CommContracts.DosageFormEnum));
+            YiBaoEnum.ItemsSource = Enum.GetValues(typeof(CommContracts.YiBaoEnum));
 
             bIsEdit = false;
             if (medicine != null)
@@ -33,6 +35,22 @@ namespace HISGUISetLib.Views
                 this.Medicine = medicine;
                 this.NameEdit.Text = medicine.Name;
                 this.MedicineTypeEnumCombo.SelectedItem = medicine.MedicineTypeEnum;
+                this.Abbr1Edit.Text = medicine.Abbr1;
+                this.Abbr2Edit.Text = medicine.Abbr2;
+                this.Abbr3Edit.Text = medicine.Abbr3;
+                this.DosageFormEnumCombo.SelectedItem = medicine.DosageFormEnum;
+                this.Unit.Text = medicine.Unit;
+                this.AdministrationRoute.Text = medicine.AdministrationRoute;
+                this.Specifications.Text = medicine.Specifications;
+                this.Manufacturer.Text = medicine.Manufacturer;
+                this.PoisonousHemp.IsChecked = medicine.PoisonousHemp;
+                this.Valuable.IsChecked = medicine.Valuable;
+                this.EssentialDrugs.IsChecked = medicine.EssentialDrugs;
+                this.YiBaoEnum.Text = medicine.YiBaoEnum.ToString();
+                this.MaxNum.Text = medicine.MaxNum.ToString();
+                this.MinNum.Text = medicine.MinNum.ToString();
+                this.SellPrice.Text = medicine.SellPrice.ToString();
+
                 bIsEdit = true;
             }
         }
@@ -52,6 +70,22 @@ namespace HISGUISetLib.Views
             {
                 Medicine.Name = this.NameEdit.Text.Trim();
                 Medicine.MedicineTypeEnum = (CommContracts.MedicineTypeEnum)this.MedicineTypeEnumCombo.SelectedItem;
+                Medicine.Abbr1 = this.Abbr1Edit.Text;
+                Medicine.Abbr2 = this.Abbr2Edit.Text;
+                Medicine.Abbr3 = this.Abbr3Edit.Text;
+                Medicine.DosageFormEnum = (CommContracts.DosageFormEnum)this.DosageFormEnumCombo.SelectedItem;
+                Medicine.Unit = this.Unit.Text;
+                Medicine.AdministrationRoute = this.AdministrationRoute.Text;
+                Medicine.Specifications = this.Specifications.Text;
+                Medicine.Manufacturer = this.Manufacturer.Text;
+                Medicine.PoisonousHemp = this.PoisonousHemp.IsChecked.Value;
+                Medicine.Valuable = this.Valuable.IsChecked.Value;
+                Medicine.EssentialDrugs = this.EssentialDrugs.IsChecked.Value;
+                Medicine.YiBaoEnum = (CommContracts.YiBaoEnum)this.YiBaoEnum.SelectedItem;
+                Medicine.MaxNum = int.Parse(this.MaxNum.Text);
+                Medicine.MinNum = int.Parse(this.MinNum.Text);
+                Medicine.SellPrice = decimal.Parse(this.SellPrice.Text);
+
 
                 CommClient.Medicine myd = new CommClient.Medicine();
                 if (myd.UpdateMedicine(Medicine))
@@ -65,7 +99,21 @@ namespace HISGUISetLib.Views
                 CommContracts.Medicine medicine = new CommContracts.Medicine();
                 medicine.Name = this.NameEdit.Text.Trim();
                 medicine.MedicineTypeEnum = (CommContracts.MedicineTypeEnum)this.MedicineTypeEnumCombo.SelectedItem;
-
+                medicine.Abbr1 = this.Abbr1Edit.Text;
+                medicine.Abbr2 = this.Abbr2Edit.Text;
+                medicine.Abbr3 = this.Abbr3Edit.Text;
+                medicine.DosageFormEnum = (CommContracts.DosageFormEnum)this.DosageFormEnumCombo.SelectedItem;
+                medicine.Unit = this.Unit.Text;
+                medicine.AdministrationRoute = this.AdministrationRoute.Text;
+                medicine.Specifications = this.Specifications.Text;
+                medicine.Manufacturer = this.Manufacturer.Text;
+                medicine.PoisonousHemp = this.PoisonousHemp.IsChecked.Value;
+                medicine.Valuable = this.Valuable.IsChecked.Value;
+                medicine.EssentialDrugs = this.EssentialDrugs.IsChecked.Value;
+                medicine.YiBaoEnum = (CommContracts.YiBaoEnum)this.YiBaoEnum.SelectedItem;
+                medicine.MaxNum = int.Parse(this.MaxNum.Text);
+                medicine.MinNum = int.Parse(this.MinNum.Text);
+                medicine.SellPrice = decimal.Parse(this.SellPrice.Text);
                 CommClient.Medicine myd = new CommClient.Medicine();
                 if (myd.SaveMedicine(medicine))
                 {
