f560c0cccb17dc72c2846e36afbcc150b2d5490b
xubei-PC\xubei <xuebei@seimun.com>
2018-01-23 09:53:04 +0800

    Add - 添加序列化的物资材料医嘱

---
+++ Contracts/MaterialDoctorAdvice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace CommContracts
+{
+    [DataContract]
+    public class MaterialDoctorAdvice : DoctorAdviceBase
+    {
+        public MaterialDoctorAdvice()
+        {
+        }
+        [DataMember]
+        public List<MaterialDoctorAdviceDetail> MaterialDoctorAdviceDetails { get; set; }
+    }
+}
---
+++ Contracts/MaterialDoctorAdviceDetail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace CommContracts
+{
+    [DataContract]
+    public class MaterialDoctorAdviceDetail : DoctorAdviceDetailBase
+    {
+        [DataMember]
+        public int MaterialID { get; set; }               // 物资材料
+        [DataMember]
+        public int MaterialDoctorAdviceID { get; set; }
+        [DataMember]
+        public MaterialItem Material { get; set; }
+    }
+}
