e57b483091aa8e7f37b686aea4bf2e551e94f0e8
xubei-PC\xubei <xuebei@seimun.com>
2018-01-23 11:29:11 +0800

    Add - 增加序列化中的化验医嘱单的类

---
+++ Contracts/AssayDoctorAdvice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace CommContracts
+{
+    [DataContract]
+    public class AssayDoctorAdvice : DoctorAdviceBase
+    {
+        public AssayDoctorAdvice()
+        {
+            AssayDoctorAdviceDetails = new List<AssayDoctorAdviceDetail>();
+        }
+        [DataMember]
+        public string ReCheckName { get; set; }
+        [DataMember]
+        public List<AssayDoctorAdviceDetail> AssayDoctorAdviceDetails { get; set; }
+    }
+}
---
+++ Contracts/AssayDoctorAdviceDetail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace CommContracts
+{
+    [DataContract]
+    public class AssayDoctorAdviceDetail : DoctorAdviceDetailBase
+    {
+        [DataMember]
+        public int AssayID { get; set; }               // 化验项目
+        [DataMember]
+        public int AssayDoctorAdviceID { get; set; }
+        [DataMember]
+        public AssayItem Assay { get; set; }
+    }
+}
