b50bde5d0e80dc5e20f9f07469fcd58d34212151
xubei-PC\xubei <xuebei@seimun.com>
2018-01-25 19:32:16 +0800

    Change - Add 升级改造号源的增删改查

--- BLL/SignalSource.cs
+++ BLL/SignalSource.cs
@@ -10,58 +10,19 @@ using System.Globalization;
 using System.Collections;
 using System.Data.Entity.Core.Objects;
 using System.Data.Entity.SqlServer;
+using AutoMapper;
 
 namespace BLL
 {
     public class SignalSource
     {
-        public List<CommContracts.SignalSource> getALLSignalSource(int DepartmentID, DateTime dateTime, int timeInterval)
-        {
-            using (DAL.HisContext ctx = new DAL.HisContext())
-            {
-                IEnumerable<DAL.SignalSource> queryResultList = from u in ctx.SignalSources
-                                                                    //where u.DepartmentID == DepartmentID &&
-                                                                    //u.TimeIntival == timeInterval &&
-                                                                where
-                                                                SqlFunctions.DateDiff("day", dateTime, u.VistTime) == 0
-                                                                select u;
-     
-
-                List<CommContracts.SignalSource> myList = new List<CommContracts.SignalSource>();
-                foreach (DAL.SignalSource tem in queryResultList)
-                {
-                    CommContracts.SignalSource temp1 = new CommContracts.SignalSource();
-
-                    temp1.ID = tem.ID;
-                    temp1.Price = tem.Price;                 // 号源单价
-                    temp1.VistTime = tem.VistTime;           // 看诊日期
-                    //temp1.TimeIntival = tem.TimeIntival;     // 看诊时段ID
-                    CommContracts.Department department = new CommContracts.Department();
-                    department.ID = DepartmentID;
-
-                    //temp1.GetDepartment = department;   // 科室
-                    //temp1.SignalType = tem.SignalType;       // 号别
-                    temp1.MaxNum = tem.MaxNum;               // 最大号源
-                    //temp1.AddMaxNum = tem.AddMaxNum;         // 临时加号号源
-                    //temp1.HasUsedNum = tem.HasUsedNum;       // 已挂号源
-                    //temp1.Specialist = tem.Specialist;       // 专家ID
-                    //temp1.Explain = tem.Explain;             // 说明
-
-
-                    myList.Add(temp1);
-                }
-                return myList;
-            }
-        }
-
         public List<DateTime> getAllSignalDate(int DepartmentID)
         {
             using (DAL.HisContext ctx = new DAL.HisContext())
             {
                 var temp = (from u in ctx.SignalSources
-                            //where u.DepartmentID == DepartmentID &&
-                            where
-                            SqlFunctions.DateDiff("day", DateTime.Now, u.VistTime) > 0
+                            where u.DepartmentID == DepartmentID &&
+                            (u.VistTime.HasValue && u.VistTime.Value.Date >= DateTime.Now.Date)
                             select DbFunctions.TruncateTime(u.VistTime)).Distinct();
 
                 List<DateTime> myList = new List<DateTime>();
@@ -188,6 +149,164 @@ namespace BLL
             //    }
             //}
             return true;
+        }
+
+        public List<CommContracts.SignalSource> GetAllSignalSource()
+        {
+            List<CommContracts.SignalSource> list = new List<CommContracts.SignalSource>();
+
+            using (DAL.HisContext ctx = new DAL.HisContext())
+            {
+                var query = from a in ctx.SignalSources
+                            select a;
+                foreach (DAL.SignalSource ass in query)
+                {
+                    var config = new MapperConfiguration(cfg =>
+                    {
+                        cfg.CreateMap<DAL.SignalSource, CommContracts.SignalSource>();
+                    });
+                    var mapper = config.CreateMapper();
+
+                    CommContracts.SignalSource temp = mapper.Map<CommContracts.SignalSource>(ass);
+                    list.Add(temp);
+                }
+            }
+            return list;
+        }
+
+        public bool SaveSignalSource(CommContracts.SignalSource signalSource)
+        {
+            using (DAL.HisContext ctx = new DAL.HisContext())
+            {
+                var config = new MapperConfiguration(cfg =>
+                {
+                    cfg.CreateMap<CommContracts.SignalSource, DAL.SignalSource>();
+                });
+                var mapper = config.CreateMapper();
+
+                DAL.SignalSource temp = new DAL.SignalSource();
+                temp = mapper.Map<DAL.SignalSource>(signalSource);
+
+                ctx.SignalSources.Add(temp);
+                try
+                {
+                    ctx.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool DeleteSignalSource(int signalSourceID)
+        {
+            using (DAL.HisContext ctx = new DAL.HisContext())
+            {
+                var temp = ctx.SignalSources.FirstOrDefault(m => m.ID == signalSourceID);
+                if (temp != null)
+                {
+                    ctx.SignalSources.Remove(temp);
+                }
+
+                try
+                {
+                    ctx.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool UpdateSignalSource(CommContracts.SignalSource signalSource)
+        {
+            using (DAL.HisContext ctx = new DAL.HisContext())
+            {
+                var temp = ctx.SignalSources.FirstOrDefault(m => m.ID == signalSource.ID);
+                if (temp != null)
+                {
+                    temp.Price = signalSource.Price;
+                    temp.VistTime = signalSource.VistTime;
+                    temp.SignalItemID = signalSource.SignalItemID;
+                    temp.EmployeeID = signalSource.EmployeeID;
+                    temp.DepartmentID = signalSource.DepartmentID;
+                }
+                else
+                {
+                    return false;
+                }
+
+                try
+                {
+                    ctx.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool SaveSignalSourceList(List<CommContracts.SignalSource> list)
+        {
+            using (DAL.HisContext ctx = new DAL.HisContext())
+            {
+                foreach (var signalSource in list)
+                {
+                    var config = new MapperConfiguration(cfg =>
+                    {
+                        cfg.CreateMap<CommContracts.SignalSource, DAL.SignalSource>();
+                    });
+                    var mapper = config.CreateMapper();
+
+                    DAL.SignalSource temp = new DAL.SignalSource();
+                    temp = mapper.Map<DAL.SignalSource>(signalSource);
+
+                    ctx.SignalSources.Add(temp);
+                }
+                try
+                {
+                    ctx.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public List<CommContracts.SignalSource> GetSignalSourceList(int DepartmentID, int EmployeeID, DateTime startDate, DateTime endDate)
+        {
+            List<CommContracts.SignalSource> list = new List<CommContracts.SignalSource>();
+
+            using (DAL.HisContext ctx = new DAL.HisContext())
+            {
+                var query = from a in ctx.SignalSources
+                            where
+                            (DepartmentID > 0 && a.DepartmentID == DepartmentID) &&
+                            (EmployeeID <= 0 || a.EmployeeID == EmployeeID) &&
+                            a.VistTime.Value <= endDate &&
+                            a.VistTime.Value >= startDate
+                            select a;
+                foreach (DAL.SignalSource ass in query)
+                {
+                    var config = new MapperConfiguration(cfg =>
+                    {
+                        cfg.CreateMap<DAL.SignalSource, CommContracts.SignalSource>();
+                    });
+                    var mapper = config.CreateMapper();
+
+                    CommContracts.SignalSource temp = mapper.Map<CommContracts.SignalSource>(ass);
+                    list.Add(temp);
+                }
+            }
+            return list;
         }
     }
 }
