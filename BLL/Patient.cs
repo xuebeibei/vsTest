fb04aeed170b59dba46b0e1775360b7b3a0a4c1a
xubei-PC\xubei <xuebei@seimun.com>
2018-01-25 19:31:06 +0800

    Add - 添加患者增删改查接口

--- BLL/Patient.cs
+++ BLL/Patient.cs
@@ -55,5 +55,111 @@ namespace BLL
 
             }
         }
+
+        public List<CommContracts.Patient> GetAllPatient(string strName)
+        {
+            List<CommContracts.Patient> list = new List<CommContracts.Patient>();
+
+            using (DAL.HisContext ctx = new DAL.HisContext())
+            {
+                var query = from t in ctx.Patients
+                            where t.Name.StartsWith(strName)
+                            select t;
+
+                var config = new MapperConfiguration(cfg =>
+                {
+                    cfg.CreateMap<DAL.Patient, CommContracts.Patient>();
+                });
+                var mapper = config.CreateMapper();
+
+                foreach (DAL.Patient tem in query)
+                {
+                    var dto = mapper.Map<CommContracts.Patient>(tem);
+                    list.Add(dto);
+                }
+            }
+
+            return list;
+        }
+
+        public bool SavePatient(CommContracts.Patient Patient)
+        {
+            using (DAL.HisContext ctx = new DAL.HisContext())
+            {
+                var config = new MapperConfiguration(cfg =>
+                {
+                    cfg.CreateMap<CommContracts.Patient, DAL.Patient>();
+                });
+                var mapper = config.CreateMapper();
+
+                DAL.Patient temp = new DAL.Patient();
+                temp = mapper.Map<DAL.Patient>(Patient);
+
+                ctx.Patients.Add(temp);
+                try
+                {
+                    ctx.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool DeletePatient(int PatientID)
+        {
+            using (DAL.HisContext ctx = new DAL.HisContext())
+            {
+                var temp = ctx.Patients.FirstOrDefault(m => m.ID == PatientID);
+                if (temp != null)
+                {
+                    ctx.Patients.Remove(temp);
+                }
+
+                try
+                {
+                    ctx.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool UpdatePatient(CommContracts.Patient Patient)
+        {
+            using (DAL.HisContext ctx = new DAL.HisContext())
+            {
+                var temp = ctx.Patients.FirstOrDefault(m => m.ID == Patient.ID);
+                if (temp != null)
+                {
+                    temp.Name = Patient.Name;
+                    temp.Gender = (DAL.GenderEnum)Patient.Gender;
+                    temp.BirthDay = Patient.BirthDay;
+                    temp.IDCardNo = Patient.IDCardNo;
+                    temp.Volk = (DAL.VolkEnum)Patient.Volk;
+                    temp.Tel = Patient.Tel;
+                    temp.JiGuan = Patient.JiGuan;
+                }
+                else
+                {
+                    return false;
+                }
+
+                try
+                {
+                    ctx.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
