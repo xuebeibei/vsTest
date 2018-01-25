b49e0f775a11cd8c084de130594087aa5ae43f8f
xubei-PC\xubei <xuebei@seimun.com>
2018-01-25 19:46:52 +0800

    Add - 添加患者增删改查界面和逻辑代码

---
+++ GUI/HISGUISetLib/Views/EditPatientView.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HISGUISetLib.Views
+{
+    /// <summary>
+    /// EditPatientView.xaml 的交互逻辑
+    /// </summary>
+    public partial class EditPatientView : UserControl
+    {
+        private bool bIsEdit;
+        private CommContracts.Patient Patient;
+        public EditPatientView(CommContracts.Patient patient = null)
+        {
+            InitializeComponent();
+            GenderCombo.ItemsSource = Enum.GetValues(typeof(CommContracts.GenderEnum));
+            VolkEnumCombo.ItemsSource = Enum.GetValues(typeof(CommContracts.VolkEnum));
+
+            bIsEdit = false;
+            if (patient != null)
+            {
+                this.Patient = patient;
+                this.Name.Text = patient.Name;
+                this.IDCardNo.Text = patient.IDCardNo;
+                this.JiGuan.Text = patient.JiGuan;
+                this.Tel.Text = patient.Tel;
+                this.GenderCombo.Text = patient.Gender.ToString();
+                this.VolkEnumCombo.Text = patient.Volk.ToString();
+                this.BirthDay.SelectedDate = patient.BirthDay;
+
+                bIsEdit = true;
+            }
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.Name.Text.Trim()))
+            {
+                return;
+            }
+
+            if (this.GenderCombo.SelectedItem == null)
+            {
+                return;
+            }
+            if (bIsEdit)
+            {
+                Patient.Name = this.Name.Text.Trim();
+                Patient.IDCardNo = this.IDCardNo.Text;
+                Patient.JiGuan = this.JiGuan.Text;
+                Patient.Tel = this.Tel.Text;
+                Patient.Gender = (CommContracts.GenderEnum)this.GenderCombo.SelectedItem;
+                Patient.Volk = (CommContracts.VolkEnum)this.VolkEnumCombo.SelectedItem;
+                Patient.BirthDay = this.BirthDay.SelectedDate;
+
+
+                CommClient.Patient myd = new CommClient.Patient();
+                if (myd.UpdatePatient(Patient))
+                {
+                    (this.Parent as Window).DialogResult = true;
+                    (this.Parent as Window).Close();
+                }
+            }
+            else
+            {
+                CommContracts.Patient patient = new CommContracts.Patient();
+                patient.Name = this.Name.Text.Trim();
+                patient.IDCardNo = this.IDCardNo.Text.Trim();
+                patient.JiGuan = this.JiGuan.Text.Trim();
+                patient.Tel = this.Tel.Text.Trim();
+                patient.Gender = (CommContracts.GenderEnum)this.GenderCombo.SelectedItem;
+                patient.Volk = (CommContracts.VolkEnum)this.VolkEnumCombo.SelectedItem;
+                patient.BirthDay = this.BirthDay.SelectedDate;
+
+                CommClient.Patient myd = new CommClient.Patient();
+                if (myd.SavePatient(patient))
+                {
+                    (this.Parent as Window).DialogResult = true;
+                    (this.Parent as Window).Close();
+                }
+            }
+        }
+
+        private void CancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            (this.Parent as Window).DialogResult = false;
+            (this.Parent as Window).Close();
+        }
+    }
+}
---
+++ GUI/HISGUISetLib/Views/PatientSetView.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition.Primitives;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Practices.ServiceLocation;
+using Prism.Regions;
+using HISGUICore;
+using HISGUISetLib.ViewModels;
+using System.Data;
+using HISGUICore.MyContorls;
+using Microsoft.Win32;
+
+namespace HISGUISetLib.Views
+{
+    [Export]
+    [Export("PatientSetView", typeof(PatientSetView))]
+    public partial class PatientSetView : HISGUIViewBase
+    {
+        public PatientSetView()
+        {
+            InitializeComponent();
+            this.Loaded += PatientSetView_Loaded;
+        }
+
+        private void PatientSetView_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdateAllDate();
+        }
+
+        [Import]
+        private HISGUISetVM ImportVM
+        {
+            set { this.VM = value; }
+        }
+
+        private void AllPatientList_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void FindItemBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string strName = this.FindItemNameBox.Text.Trim();
+            UpdateAllDate(strName);
+        }
+
+        private void NewItemBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // 新增患者
+            var window = new Window();
+
+            EditPatientView eidtPatient = new EditPatientView();
+            window.Content = eidtPatient;
+            window.Width = 400;
+            window.Height = 500;
+            window.ResizeMode = ResizeMode.NoResize;
+            bool? bResult = window.ShowDialog();
+
+            if (bResult.Value)
+            {
+                MessageBox.Show("患者新建完成！");
+                UpdateAllDate();
+            }
+        }
+
+        private void DeleteItemBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var currentPatient = this.AllPatientList.SelectedItem as CommContracts.Patient;
+            if (currentPatient == null)
+                return;
+
+            if (MessageBox.Show("确认删除该患者？", "删除", MessageBoxButton.OKCancel) == MessageBoxResult.OK)
+            {
+                var vm = this.DataContext as HISGUISetVM;
+                bool? bIsOK = vm?.DeletePatient(currentPatient.ID);
+                if (bIsOK.HasValue)
+                {
+                    if (bIsOK.Value)
+                    {
+                        MessageBox.Show("患者删除完成！");
+                        UpdateAllDate();
+                    }
+                }
+            }
+        }
+
+        private void EditItemBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var temp = this.AllPatientList.SelectedItem as CommContracts.Patient;
+            if (temp == null)
+                return;
+
+            // 新增患者
+            var window = new Window();
+
+            EditPatientView eidtPatient = new EditPatientView(temp);
+            window.Content = eidtPatient;
+            window.Width = 400;
+            window.Height = 500;
+            window.ResizeMode = ResizeMode.NoResize;
+            bool? bResult = window.ShowDialog();
+
+            if (bResult.Value)
+            {
+                MessageBox.Show("患者修改完成！");
+                UpdateAllDate();
+            }
+        }
+
+        private void ExportItemBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ImportItemBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UpdateAllDate(string strName = "")
+        {
+            var vm = this.DataContext as HISGUISetVM;
+            this.AllPatientList.ItemsSource = vm?.GetAllPatient(strName);
+        }
+    }
+}
