e4e42d600adb5c552d45d9eacdaf195461d4969d
xubei-PC\xubei <xuebei@seimun.com>
2018-01-25 19:45:03 +0800

    Change - 修改挂号的逻辑

--- GUI/HISGUIFeeLib/Views/RegistrationView.xaml.cs
+++ GUI/HISGUIFeeLib/Views/RegistrationView.xaml.cs
@@ -22,13 +22,38 @@ using System.Data;
 
 namespace HISGUIFeeLib.Views
 {
+    public class SignalSourceMsg
+    {
+        public SignalSourceMsg()
+        {
+            TimeEnum = CommContracts.SignalTimeEnum.上午;
+        }
+
+        public SignalSourceMsg(CommContracts.SignalTimeEnum signalTimeEnum)
+        {
+            TimeEnum = signalTimeEnum;
+        }
+
+        public CommContracts.SignalTimeEnum TimeEnum { get; set; }
+        public string Monday { get; set; }
+        public string Tuesday { get; set; }
+        public string Wednesday { get; set; }
+        public string Thursday { get; set; }
+        public string Friday { get; set; }
+        public string Saturday { get; set; }
+        public string Sunday { get; set; }
+    }
     [Export]
     [Export("RegistrationView", typeof(RegistrationView))]
     public partial class RegistrationView : HISGUIViewBase
     {
+        private decimal? currentPatientBalance;
         public RegistrationView()
         {
             InitializeComponent();
+            currentPatientBalance = 0.0m;
+            updateSignalSourceMsg();
+            this.PayWayCombo.ItemsSource = Enum.GetValues(typeof(CommContracts.PayWayEnum));
             this.Loaded += RegistrationView_Loaded;
         }
 
@@ -36,13 +61,6 @@ namespace HISGUIFeeLib.Views
         {
             var vm = this.DataContext as HISGUIFeeVM;
             this.departmentList.ItemsSource = vm?.getAllDepartment();//数据绑定
-
-            //vm?.newRegistrationBill();
-            //vm?.getPatient();
-            //vm?.initSignalTime();
-
-            //vm?.showDepartmentSignal(0);
-            //this.grid1.ItemsSource = vm._dt.DefaultView;
         }
 
         [Import]
@@ -51,87 +69,25 @@ namespace HISGUIFeeLib.Views
             set { this.VM = value; }
         }
 
-        private void ShowList(string row, string column)
-        {
-            DateTime date = Convert.ToDateTime(column);
-
-            var vm = this.DataContext as HISGUIFeeVM;
-
-            var aaa = this.departmentList.SelectedItem as CommContracts.Department;
-
-            //this.listView1.ItemsSource = vm.getSignalSource(aaa.ID, date, Convert.ToInt32(row));
-
-        }
-
-        private void listView1_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            //SignalSource temp = this.listView1.SelectedItem as SignalSource;
-            //var vm = this.DataContext as HISGUIFeeVM;
-            //vm?.showSelectSignal(temp);
-        }
-
-        private void Button_Click_21(object sender, RoutedEventArgs e)
-        {
-            var vm = this.DataContext as HISGUIFeeVM;
-            //bool? saveResult = vm?.SaveRegistrationBill();
-            //if (!saveResult.HasValue)
-            //{
-            //    MessageBox.Show("保存失败！");
-            //    return;
-            //}
-            //else if ((bool)saveResult.Value)
-            //{
-            //    MessageBox.Show("保存成功！");
-            //    vm?.newRegistrationBill();
-            //}
-            //else
-            //{
-            //    MessageBox.Show("保存失败！");
-            //    return;
-            //}
-        }
-
-        private void grid1_SelectedCellsChanged_1(object sender, SelectedCellsChangedEventArgs e)
-        {
-            DataGrid dg = sender as DataGrid;
-
-            try
-            {
-                //string columnIndex = dg.SelectedCells[0].Column.ToString();  // 列坐标
-                //var rowIdnex = dg.Items.IndexOf(dg.SelectedCells[0].Item);   // 行坐标
-                string data = dg.SelectedCells[0].Column.Header.ToString();
-                var temp = dg.SelectedCells[0].Item as DataRowView;
-                var index = temp[0].ToString();
-
-
-                ShowList(index, data);
-            }
-            catch (Exception ex)
-            {
-
-            }
-
-        }
-
         private void ReadCardBtn_Click(object sender, RoutedEventArgs e)
         {
             var vm = this.DataContext as HISGUIFeeVM;
-            CommContracts.Patient patient = vm?.ReadCurrentPatient(8);
-            decimal? dBalance = vm?.GetCurrentPatientBalance(8);
+            CommContracts.Patient patient = vm?.ReadCurrentPatient(1);
+            currentPatientBalance = vm?.GetCurrentPatientBalance(1);
             if (patient == null)
                 return;
             PatientMsg.Inlines.Clear();
             string str =
-                "姓名：" + patient.Name + "\r\n" +
-                "性别：" + patient.Gender + "\r\n" +
-                "生日：" + patient.BirthDay + "\r\n" +
-                "身份证号：" + patient.IDCardNo + "\r\n" +
-                "民族：" + patient.Volk + "\r\n" +
-                "籍贯：" + patient.JiGuan + "\r\n" +
-                "电话：" + patient.Tel + "\r\n"
+                "姓名：" + patient.Name + " " +
+                "性别：" + patient.Gender + " " +
+                "生日：" + patient.BirthDay + " " +
+                "身份证号：" + patient.IDCardNo + " " +
+                "民族：" + patient.Volk + " " +
+                "籍贯：" + patient.JiGuan + " " +
+                "电话：" + patient.Tel + " "
                 ;
             PatientMsg.Inlines.Add(new Run(str));
-            PatientMsg.Inlines.Add(new Run("账户余额：" + dBalance.Value) { Foreground = Brushes.Green, FontSize = 25 });
+            PatientMsg.Inlines.Add(new Run("账户余额：" + currentPatientBalance.Value) { Foreground = Brushes.Green, FontSize = 25 });
 
         }
 
@@ -143,10 +99,268 @@ namespace HISGUIFeeLib.Views
         private void departmentList_Selected(object sender, RoutedEventArgs e)
         {
             var department = this.departmentList.SelectedItem as CommContracts.Department;
-            DataTable data = new DataTable();
+            if (department == null)
+                return;
+
+            updateSignalSourceMsg();
+            updateSignalList();
+        }
+        private void updateSignalSourceMsg()
+        {
+            initSignalSourceGrid();
+            this.SignalSourceGrid.ItemsSource = updateSignalSourceGrid();
+        }
+
+        private List<SignalSourceMsg> updateSignalSourceGrid()
+        {
+            List<SignalSourceMsg> nullData = new List<SignalSourceMsg>();
+            nullData.Add(new SignalSourceMsg(CommContracts.SignalTimeEnum.上午));
+            nullData.Add(new SignalSourceMsg(CommContracts.SignalTimeEnum.下午));
+            nullData.Add(new SignalSourceMsg(CommContracts.SignalTimeEnum.晚上));
+
+            var department = this.departmentList.SelectedItem as CommContracts.Department;
+            if (department == null)
+                return nullData;
+
             var vm = this.DataContext as HISGUIFeeVM;
-            data = vm?.showDepartmentSignal(department.ID);
-            this.grid1.ItemsSource = data.DefaultView;
+
+            List<SignalSourceMsg> data = new List<SignalSourceMsg>();
+
+            List<CommContracts.SignalSource> sourceList = vm?.GetDepartmentSignalSourceList(department.ID, DateTime.Now.Date, DateTime.Now.AddDays(6).Date);
+            bool bIsHasRegistration = false;
+            List<CommContracts.Registration> registrationList = vm?.GetDepartmentRegistrationList(department.ID, DateTime.Now.Date, DateTime.Now.AddDays(6).Date);
+            if (!(registrationList == null || registrationList.Count <= 0))
+            {
+                bIsHasRegistration = true;
+            }
+
+            if (sourceList == null || sourceList.Count <= 0)
+            {
+                return nullData;
+            }
+            else
+            {
+                foreach (CommContracts.SignalTimeEnum tim in Enum.GetValues(typeof(CommContracts.SignalTimeEnum)))
+                {
+                    SignalSourceMsg msg = new SignalSourceMsg(tim);
+                    foreach (DayOfWeek day in Enum.GetValues(typeof(DayOfWeek)))
+                    {
+                        var query = from u in sourceList
+                                    where u.VistTime.Value.DayOfWeek == day &&
+                                    u.SignalItem.SignalTimeEnum == tim
+                                    select u.MaxNum;
+                        string str = (query.Sum() <= 0 ? "" : query.Sum().ToString());
+                        if (bIsHasRegistration)
+                        {
+                            var regisQuery = from e in registrationList
+                                             where e.SignalSource.VistTime.Value.DayOfWeek == day &&
+                                             e.SignalSource.SignalItem.SignalTimeEnum == tim
+                                             select e;
+                            str += ("|" + regisQuery.Count().ToString());
+                        }
+
+                        switch (day)
+                        {
+                            case DayOfWeek.Monday:
+                                {
+                                    msg.Monday = str;
+                                    break;
+                                }
+                            case DayOfWeek.Tuesday:
+                                {
+                                    msg.Tuesday = str;
+                                    break;
+                                }
+                            case DayOfWeek.Wednesday:
+                                {
+                                    msg.Wednesday = str;
+                                    break;
+                                }
+                            case DayOfWeek.Thursday:
+                                {
+                                    msg.Thursday = str;
+                                    break;
+                                }
+                            case DayOfWeek.Saturday:
+                                {
+                                    msg.Saturday = str;
+                                    break;
+                                }
+                            case DayOfWeek.Sunday:
+                                {
+                                    msg.Sunday = str;
+                                    break;
+                                }
+                            case DayOfWeek.Friday:
+                                {
+                                    msg.Friday = str;
+                                    break;
+                                }
+                            default:
+                                break;
+                        }
+                    }
+                    data.Add(msg);
+                }
+            }
+            return data;
+        }
+
+        private void initSignalSourceGrid()
+        {
+            this.SignalSourceGrid.Columns.Clear();
+
+            DataGridLength length = new DataGridLength(1, DataGridLengthUnitType.Star);
+
+            this.SignalSourceGrid.Columns.Add(new DataGridTextColumn()
+            {
+                Header = "时段\\日期",
+                Binding = new Binding("TimeEnum"),
+                Width = 63,
+                IsReadOnly = true
+                //,
+                //Foreground = Brushes.Blue // 设置该列字体颜色
+            });
+
+            for (int i = 0; i < 7; i++)
+            {
+                DateTime tempDate = DateTime.Now.AddDays(i);
+                this.SignalSourceGrid.Columns.Add(new DataGridTextColumn()
+                {
+                    Header = tempDate.ToString("yyyy-MM-dd dddd") + (tempDate.Date == DateTime.Now.Date ? "*" : ""),
+                    Binding = new Binding(tempDate.DayOfWeek.ToString()),
+                    Width = length,
+                    IsReadOnly = (tempDate.Date >= DateTime.Now.Date ? false : true)
+                });
+            }
+
+        }
+
+        private void SignalSourceGrid_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+            updateSignalList();
+        }
+
+        private void updateSignalList()
+        {
+            SignalList.ItemsSource = null;
+            if (SignalSourceGrid.SelectedCells == null || SignalSourceGrid.SelectedCells.Count <= 0)
+                return;
+            string data = SignalSourceGrid.SelectedCells[0].Column.Header.ToString();
+            int rowIdnex = SignalSourceGrid.Items.IndexOf(SignalSourceGrid.SelectedCells[0].Item);   // 行坐标
+
+            DateTime dt = DateTime.ParseExact(data, "yyyy-MM-dd dddd", System.Globalization.CultureInfo.CurrentCulture);
+            if (dt == null)
+                return;
+            List<SignalSourceMsg> list = SignalSourceGrid.ItemsSource as List<SignalSourceMsg>;
+
+            if (list != null)
+            {
+                CommContracts.SignalTimeEnum timeEnum = list.ElementAt(rowIdnex).TimeEnum;
+
+                var department = this.departmentList.SelectedItem as CommContracts.Department;
+                if (department == null)
+                    return;
+
+                var vm = this.DataContext as HISGUIFeeVM;
+                List<CommContracts.SignalSource> sourceList = vm?.GetDepartmentSignalSourceList(department.ID, dt, dt);
+                SignalList.ItemsSource = sourceList;
+            }
+        }
+
+        private void AddCheck_Click(object sender, RoutedEventArgs e)
+        {
+            this.PayBtn.Visibility = Visibility.Visible;
+            this.ReturnBtn.Visibility = Visibility.Collapsed;
+            this.PayPanel.Visibility = Visibility.Visible;
+            this.ReturnPanel.Visibility = Visibility.Collapsed;
+            this.EditGrid.Visibility = Visibility.Visible;
+        }
+
+        private void DeleteCheck_Click(object sender, RoutedEventArgs e)
+        {
+            this.PayBtn.Visibility = Visibility.Collapsed;
+            this.ReturnBtn.Visibility = Visibility.Visible;
+            this.PayPanel.Visibility = Visibility.Collapsed;
+            this.ReturnPanel.Visibility = Visibility.Visible;
+            this.EditGrid.Visibility = Visibility.Collapsed;
+        }
+
+        private void SignalList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var tem = this.SignalList.SelectedItem as CommContracts.SignalSource;
+            if (tem == null)
+                return;
+
+            this.DiscountEdit.Text = 0.0.ToString();
+            this.DuePayMoneyEdit.Text = tem.Price.ToString();
+            if(currentPatientBalance.Value>=tem.Price)
+            {
+                this.PayWayCombo.SelectedItem = CommContracts.PayWayEnum.账户支付;
+            }
+            else
+            {
+                this.PayWayCombo.SelectedItem = CommContracts.PayWayEnum.现金支付;
+            }
+
+            this.RealPayMoneyEdit.Focus();
+        }
+
+        private void RealPayMoneyEdit_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            decimal Discount = 0, DuePay = 0, RealPay = 0, charge = 0;
+            Discount = string.IsNullOrEmpty(this.DiscountEdit.Text.Trim()) ? 0.0m : decimal.Parse(this.DiscountEdit.Text);
+            DuePay = string.IsNullOrEmpty(this.DuePayMoneyEdit.Text.Trim()) ? 0.0m : decimal.Parse(this.DuePayMoneyEdit.Text);
+            RealPay = string.IsNullOrEmpty(this.RealPayMoneyEdit.Text.Trim()) ? 0.0m : decimal.Parse(this.RealPayMoneyEdit.Text);
+            charge = RealPay - Discount - DuePay;
+
+            this.ChargeMoneyEdit.Text = charge.ToString();
+        }
+
+        private void RealPayMoneyEdit_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Return||e.Key == Key.Enter)
+            {
+                this.PayBtn.Focus();
+            }
+        }
+
+        private void PayBtn_Click(object sender, RoutedEventArgs e)
+        {
+            CommContracts.Registration registration = new CommContracts.Registration();
+            registration.PatientID = 1;
+            
+            registration.PayWayEnum = (CommContracts.PayWayEnum)this.PayWayCombo.SelectedItem;
+            registration.RegisterFee = string.IsNullOrEmpty(this.DuePayMoneyEdit.Text.Trim()) ? 0.0m : decimal.Parse(this.DuePayMoneyEdit.Text);
+            registration.RegisterTime = DateTime.Now;
+            registration.RegisterUserID = 1;
+            var signal = this.SignalList.SelectedItem as CommContracts.SignalSource;
+            if (signal == null)
+                return;
+
+            registration.SignalSourceID = signal.ID;
+
+            var vm = this.DataContext as HISGUIFeeVM;
+            bool? result = vm.SaveRegistration(registration);
+            if(result.HasValue)
+            {
+                if(result.Value)
+                {
+                    MessageBox.Show("挂号成功！");
+                    return;
+                }
+            }
+            MessageBox.Show("挂号失败！");
+        }
+
+        private void ReturnBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
