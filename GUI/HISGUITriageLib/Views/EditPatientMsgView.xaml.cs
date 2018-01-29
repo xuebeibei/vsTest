5aa6d4e91896aa1c61575215078fb34175ac826e
xubei-PC\xubei <xuebei@seimun.com>
2018-01-29 09:51:21 +0800

    Add - 到诊分诊暂时结束，看诊开始，串联数据

--- GUI/HISGUITriageLib/Views/EditPatientMsgView.xaml.cs
+++ GUI/HISGUITriageLib/Views/EditPatientMsgView.xaml.cs
@@ -27,15 +27,89 @@ namespace HISGUINurseLib.Views
     [Export("EditPatientMsgView", typeof(EditPatientMsgView))]
     public partial class EditPatientMsgView : HISGUIViewBase
     {
+        //private bool bIsEdit;
+        //private CommContracts.SickBed SickBed;
+
         public EditPatientMsgView()
         {
             InitializeComponent();
+            //CommClient.Department myd = new CommClient.Department();
+            //CommClient.SickRoom myd1 = new CommClient.SickRoom();
+
+            //DepartmentCombo.ItemsSource = myd.getALLDepartment("");
+            //SickRoomCombo.ItemsSource = myd1.GetAllSickRoom();
+            //bIsEdit = false;
+            //if (sickBed != null)
+            //{
+            //    this.SickBed = sickBed;
+            //    this.NameEdit.Text = sickBed.Name;
+            //    this.RemarksEdit.Text = sickBed.Remarks;
+            //    this.DepartmentCombo.SelectedItem = sickBed.SickRoom.Department;
+            //    this.SickRoomCombo.SelectedItem = sickBed.SickRoom;
+            //    bIsEdit = true;
+            //}
         }
+
 
         [Import]
         private HISGUINurseVM ImportVM
         {
             set { this.VM = value; }
+        }
+
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //if (string.IsNullOrEmpty(this.NameEdit.Text.Trim()))
+            //{
+            //    return;
+            //}
+
+            //if (this.DepartmentCombo.SelectedItem == null)
+            //{
+            //    return;
+            //}
+
+            //if (this.SickRoomCombo.SelectedItem == null)
+            //{
+            //    return;
+            //}
+
+            //if (bIsEdit)
+            //{
+            //    SickBed.Name = this.NameEdit.Text.Trim();
+            //    SickBed.Remarks = this.RemarksEdit.Text.Trim();
+            //    SickBed.SickRoom = (CommContracts.SickRoom)this.SickRoomCombo.SelectedItem;
+
+
+            //    CommClient.SickBed myd = new CommClient.SickBed();
+            //    if (myd.UpdateSickBed(SickBed))
+            //    {
+            //        (this.Parent as Window).DialogResult = true;
+            //        (this.Parent as Window).Close();
+            //    }
+            //}
+            //else
+            //{
+            //    CommContracts.SickBed sickBed = new CommContracts.SickBed();
+            //    sickBed.Name = this.NameEdit.Text.Trim();
+            //    sickBed.Remarks = this.RemarksEdit.Text.Trim();
+            //    sickBed.SickRoom = (CommContracts.SickRoom)this.SickRoomCombo.SelectedItem;
+
+
+            //    CommClient.SickBed myd = new CommClient.SickBed();
+            //    if (myd.SaveSickBed(sickBed))
+            //    {
+            //        (this.Parent as Window).DialogResult = true;
+            //        (this.Parent as Window).Close();
+            //    }
+            //}
+        }
+
+        private void CancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            (this.Parent as Window).DialogResult = false;
+            (this.Parent as Window).Close();
         }
     }
 }
