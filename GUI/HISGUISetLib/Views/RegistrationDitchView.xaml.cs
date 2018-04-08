df51470f57968e0e263bd3b4f9cdd3d2b34b3f58
xubei-PC\xubei <xuebei@seimun.com>
2018-04-08 10:54:52 +0800

    Add - 添加对服务分离后，挂号渠道的数据接口的测试

--- GUI/HISGUISetLib/Views/RegistrationDitchView.xaml.cs
+++ GUI/HISGUISetLib/Views/RegistrationDitchView.xaml.cs
@@ -31,21 +31,61 @@ namespace HISGUISetLib.Views
         public RegistrationDitchView()
         {
             InitializeComponent();
+            this.Loaded += RegistrationDitchView_Loaded;
+        }
+
+        [Import]
+        private HISGUISetVM ImportVM
+        {
+            set { this.VM = value; }
+        }
+
+        private void RegistrationDitchView_Loaded(object sender, RoutedEventArgs e)
+        {
+            updateAllRegistrationDitchList();
         }
 
         private void NewBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            CommContracts.RegistrationDitch vistTime = new CommContracts.RegistrationDitch();
+            updateDateToView(vistTime);
+            EditGrid.IsEnabled = true;
+            this.NameBox.Focus();
+            if (this.AllRegistrationDitchViewList.SelectedItem != null)
+                this.AllRegistrationDitchViewList.SelectedItem = null;
         }
 
         private void SaveBtn_Click(object sender, RoutedEventArgs e)
         {
+            var vm = this.DataContext as HISGUISetVM;
+            CommClient.RegistrationDitch vistTimeClient = new CommClient.RegistrationDitch();
 
+            if(vm.CurrentRegistrationDitch == null)
+            {
+                CommContracts.RegistrationDitch ditch = new CommContracts.RegistrationDitch();
+                vm.CurrentRegistrationDitch = ditch;
+            }
+            vm.CurrentRegistrationDitch.Name = this.NameBox.Text;
+            vm.CurrentRegistrationDitch.Priority = 0;
+            vm.CurrentRegistrationDitch.Proportion = int.Parse(this.ProportionBox.Text);
+            //vm.CurrentRegistrationDitch.Status = bool.Parse(this.StatusBox.Text);
+            vm.CurrentRegistrationDitch.Status = true;
+
+
+            if (vistTimeClient.SaveRegistrationDitch(vm.CurrentRegistrationDitch))
+            {
+                MessageBox.Show("OK");
+                updateAllRegistrationDitchList();
+            }
+            else
+            {
+                MessageBox.Show("Error");
+            }
         }
 
         private void EditBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            EditGrid.IsEnabled = true;
         }
 
         private void DeleteBtn_Click(object sender, RoutedEventArgs e)
@@ -70,7 +110,26 @@ namespace HISGUISetLib.Views
 
         private void AllRegistrationDitchViewList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            var currentItem = this.AllRegistrationDitchViewList.SelectedItem as CommContracts.RegistrationDitch;
+            if (currentItem == null)
+                return;
 
+            updateDateToView(currentItem);
+
+            EditGrid.IsEnabled = false;
+        }
+
+        private void updateDateToView(CommContracts.RegistrationDitch RegistrationDitch)
+        {
+            var vm = this.DataContext as HISGUISetVM;
+            vm.CurrentRegistrationDitch = RegistrationDitch;
+        }
+
+        private void updateAllRegistrationDitchList()
+        {
+            CommClient.RegistrationDitch registrationDitchClient = new CommClient.RegistrationDitch();
+            List<CommContracts.RegistrationDitch> list = registrationDitchClient.GetAllRegistrationDitch();
+            this.AllRegistrationDitchViewList.ItemsSource = list;
         }
     }
 }
